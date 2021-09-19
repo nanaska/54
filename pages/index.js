e8eadf292f8c1a1151740a55adb70ae9bb4d8989
you@example.com <you@example.com>
2021-09-19 21:08:09 +0300

    <test>

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://vk.com/dsshakhov0">Денис!</a>
         </h1>
 
         <p className={styles.description}>
