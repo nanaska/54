14731675b9cd762fbdbc13a1764f54909f388742
you@example.com <you@example.com>
2021-09-19 21:50:19 +0300

    marina

---
+++ pages/dersiption.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+const dersiption = () => {
+    return (
+        <div className={styles.descrption_page}>
+        <div className={styles.poseredine}>
+            <div className={styles.fag}>Потому что я ее люблю. Еще она класная эта самое прикольная красивая и умная🌹💕</div>
+        </div>
+        </div>
+    )
+}
+
+export default dersiption
---
+++ pages/learn.js
@@ -0,0 +1,37 @@
+
+import styles from '../styles/Home.module.css'
+const learn = () => {
+    return (
+<main className={styles.main}>
+        
+
+        <div className={styles.grid}>
+          
+          <a  className={styles.card}>
+            <image src="../public/69fSdGUkSu8.jpg"/>
+            
+          </a>
+          
+          <a className={styles.card}>
+           
+          </a>
+
+          <a
+           
+            className={styles.card}
+          >
+            
+          </a>
+
+          <a
+            
+            className={styles.card}
+          >
+           
+          </a>
+        </div>
+      </main>
+    )
+}
+
+export default learn
